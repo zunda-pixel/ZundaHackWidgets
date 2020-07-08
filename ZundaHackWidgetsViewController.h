f2b187003459b780fad806b71541593e9985d490
zunda-pixel <zunda.pixel@gmail.com>
2020-07-08 16:12:41 +0900

    remove SubView

--- ZundaHackWidgetsViewController.h
+++ ZundaHackWidgetsViewController.h
@@ -1,8 +1,5 @@
 #import <HSWidgets/HSWidgetViewController.h>
 
 @interface ZundaHackWidgetsViewController : HSWidgetViewController
-
-@property (nonatomic, strong) UIView *subView;
-
 @end
 
--- ZundaHackWidgetsViewController.mm
+++ ZundaHackWidgetsViewController.mm
@@ -3,25 +3,21 @@
 @implementation ZundaHackWidgetsViewController
 -(void)viewDidLoad {
     [super viewDidLoad];
-    // add custom view
-    self.subView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 60, 60)];
 
-    NSString *path = @"Library/HSWidgets/TestWidgets.bundle/icon.png";
+    NSString *path = @"Library/HSWidgets/ZundaHackWidgets.bundle/icon.png";
     UIImage *image = [UIImage imageWithContentsOfFile:path];
 
     UIButton *button = [[UIButton alloc] init];
-    [button setBackgroundImage:image forState:UIControlStateNormal];
     button.frame = CGRectMake(0, 0, 60, 60);
 
+    [button setBackgroundImage:image forState:UIControlStateNormal];
     [button addTarget:self action:@selector(OpenSite) forControlEvents:UIControlEventTouchDown];
-
-    [self.subView addSubview:button];
-    [self.view addSubview: self.subView];
     
+    [self.view addSubview: button];
 }
 
 +(HSWidgetSize)minimumSize {
-    return HSWidgetSizeMake(1, 1); // least amount of rows and cols the widget needs
+    return HSWidgetSizeMake(1, 1);
 }
 
 -(void)OpenSite {
